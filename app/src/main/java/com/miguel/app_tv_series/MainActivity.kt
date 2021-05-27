347c442e655746559f6df8c61ac2a137c13fb876
Miguel <miguelantoliv@gmail.com>
2021-05-27 20:21:48 -0300

    First commit.

---
+++ app/src/main/java/com/miguel/app_tv_series/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miguel.app_tv_series
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
